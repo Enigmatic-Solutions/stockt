b51915900b0dae94e43f1a518b531e8548b81d34
RamithaSilva <97995271+RamithaSilva@users.noreply.github.com>
2022-05-27 13:18:54 +0530

    update mid 5/27

--- app/src/main/java/stock/app/view/activities/MainActivity.kt
+++ app/src/main/java/stock/app/view/activities/MainActivity.kt
@@ -22,15 +22,13 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
 
-    lateinit var toggle:ActionBarDrawerToggle
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //supportActionBar(customtoolbar)
+
 
         val toolbar_img1:ImageView=findViewById(R.id.menu_icon)
         val toolbar_img2:ImageView=findViewById(R.id.login_icon)
@@ -65,19 +63,23 @@ class MainActivity : AppCompatActivity() {
         val drawyerLayout:DrawerLayout=findViewById(R.id.drawerlayout)
         val navView:NavigationView=findViewById(R.id.navView)
 
-        toggle= ActionBarDrawerToggle(this,drawyerLayout,R.string.open,R.string.close)
-
-        Toast.makeText(this,"Menu Icon Selected",Toast.LENGTH_SHORT).show()
-        drawyerLayout.addDrawerListener(toggle)
-        toggle.syncState()
 
         //supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
 
         toolbar_img1.setOnClickListener{
-
+            drawyerLayout.open()
+            Toast.makeText(this,"Menu Icon Selected",Toast.LENGTH_SHORT).show()
         }
 
 
+        navView.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_home -> Toast.makeText(this,"Home Icon Selected",Toast.LENGTH_SHORT).show()
+                R.id.nav_stock -> Toast.makeText(this,"Stock Icon Selected",Toast.LENGTH_SHORT).show()
+            }
+
+            true
+        }
 
 
     }
